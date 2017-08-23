15e226d0962b8d9950feff6f5ea76b6badab4fa8
qsftest123 <qishifan1991@163.com>
2017-08-23 16:37:15 +0800

    添加地址选择器

---
+++ qsfutils/src/main/java/com/qsfan/qsfutils/picker/AddressInitTask.java
@@ -0,0 +1,128 @@
+package com.qsfan.qsfutils.picker;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+
+import cn.qqtheme.framework.picker.AddressPicker;
+
+
+/** 
+ * ==============版权所有===============
+ *  
+ * 类注释: 获取地址数据并显示地址选择器
+ * 作者: QSFan
+ * 邮箱: qsfan_vip.163.com
+ * 时间: 2017/8/23 16:31 
+ * 版本: 
+ * 备注：
+ * ==============版权所有===============
+*/
+public class AddressInitTask extends AsyncTask<String, Void, ArrayList<AddressPicker.Province>> {
+    private Activity activity;
+    private ProgressDialog dialog;
+    private String selectedProvince = "", selectedCity = "", selectedCounty = "";
+    private boolean hideCounty = false;
+
+    /**
+     * 初始化为不显示区县的模式
+     *
+     * @param activity
+     * @param hideCounty is hide County
+     */
+    public AddressInitTask(Activity activity, boolean hideCounty) {
+        this.activity = activity;
+        this.hideCounty = hideCounty;
+        dialog = ProgressDialog.show(activity, null, "正在初始化数据...", true, true);
+    }
+
+    public AddressInitTask(Activity activity) {
+        this.activity = activity;
+        dialog = ProgressDialog.show(activity, null, "正在初始化数据...", true, true);
+    }
+
+    @Override
+    protected ArrayList<AddressPicker.Province> doInBackground(String... params) {
+        if (params != null) {
+            switch (params.length) {
+                case 1:
+                    selectedProvince = params[0];
+                    break;
+                case 2:
+                    selectedProvince = params[0];
+                    selectedCity = params[1];
+                    break;
+                case 3:
+                    selectedProvince = params[0];
+                    selectedCity = params[1];
+                    selectedCounty = params[2];
+                    break;
+                default:
+                    break;
+            }
+        }
+        ArrayList<AddressPicker.Province> data = new ArrayList<AddressPicker.Province>();
+        try {
+            String json = AssetsUtils.readText(activity, "city.json");
+            data.addAll(JSON.parseArray(json, AddressPicker.Province.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<AddressPicker.Province> result) {
+        dialog.dismiss();
+        if (result.size() > 0) {
+            AddressPicker picker = new AddressPicker(activity, result);
+            picker.setHideCounty(hideCounty);
+            picker.setSelectedItem(selectedProvince, selectedCity, selectedCounty);
+            picker.setOnAddressPickListener(new AddressPicker.OnAddressPickListener() {
+                @Override
+                public void onAddressPicked(AddressPicker.Province province, AddressPicker.City city, AddressPicker.County county) {
+                    if (county == null) {
+                    } else {
+                        String sheng = province.getAreaName();
+                        String shi = city.getAreaName();
+                        String xian = county.getAreaName();
+                        String shiId = city.getAreaId();
+                        String xianID = county.getAreaId();
+                        String address = sheng + shi + xian;
+                        onAddressListener.onAdressListener(sheng, shi, xian,shiId,xianID);
+//                        setOnAddressListener(onAddressListener);
+                    }
+                }
+            });
+            picker.show();
+        } else {
+            Toast.makeText(activity, "数据初始化失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public interface OnAddressListener {
+        /**
+         *
+         * @param sheng 省
+         * @param shi   市
+         * @param qu    县（区）
+         * @param shiID 市ID
+         */
+        void onAdressListener(String sheng, String shi, String qu, String shiID, String xianID);
+
+    }
+
+    private OnAddressListener onAddressListener;//设置地址回调监听
+
+
+    public void setOnAddressListener(OnAddressListener onAddressListener) {
+        this.onAddressListener = onAddressListener;
+
+    }
+
+}
